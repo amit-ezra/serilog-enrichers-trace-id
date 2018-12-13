0568c78fbb87ecbf6e85a46f0b3325255fae0540
Amit Ezra <amit.ezra@Genesys.com>
2018-12-13 18:07:10 +0100

    create TraceIdEnricher

---
+++ SerilogTraceIdEnricher/TraceIdEnricher.cs
@@ -0,0 +1,37 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+using Microsoft.AspNetCore.Http;
+
+namespace Serilog.Enrichers
+{
+  public class TraceIdEnricher : ILogEventEnricher
+  {
+    private const string propertyName = "TraceId";
+    private readonly IHttpContextAccessor httpContextAccessor;
+
+    public TraceIdEnricher()
+      : this(new HttpContextAccessor())
+    {
+    }
+
+    public TraceIdEnricher(IHttpContextAccessor httpContextAccessor)
+    {
+      this.httpContextAccessor = httpContextAccessor;
+    }
+
+    public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+    {
+      var traceId = GetTraceId();
+      if (!string.IsNullOrWhiteSpace(traceId))
+      {
+        var correlationIdProperty = new LogEventProperty(propertyName, new ScalarValue(traceId));
+        logEvent.AddOrUpdateProperty(correlationIdProperty);
+      }
+    }
+
+    private string GetTraceId()
+    {
+      return httpContextAccessor?.HttpContext?.TraceIdentifier;
+    }
+  }
+}
---
+++ SerilogTraceIdEnricher/TraceIdLoggerConfigurationExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Serilog.Configuration;
+using Serilog.Enrichers;
+
+namespace Serilog
+{
+  public static class TraceIdLoggerConfigurationExtension
+  {
+    public static LoggerConfiguration WithTraceId(this LoggerEnrichmentConfiguration enrichmentConfiguration)
+    {
+      if (enrichmentConfiguration == null) throw new ArgumentNullException(nameof(enrichmentConfiguration));
+      return enrichmentConfiguration.With<TraceIdEnricher>();
+    }
+  }
+}
